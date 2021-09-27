8d3237ce20476cd9edb028e67aa2c6b021de4e8f
Laurent Ellerbach <laurelle@microsoft.com>
2021-09-27 13:13:33 +0300

    Adjusting nugets a bit, adding cmd line options, adding message helper

--- Program.cs
+++ Program.cs
@@ -1,32 +1,83 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using ICSharpCode.Decompiler;
-using ICSharpCode.Decompiler.CSharp;
 using nanoFramework.Tools.Debugger;
-using nanoFramework.Tools.Debugger.Extensions;
 using System.IO;
-using System.Xml;
+using nanoFrameworkFlasher.Helpers;
+using CommandLine;
+using System.Linq;
 
 namespace nanoFrameworkFlasher
 {
     class Program
     {
-        static async Task Main(string[] args)
+        private static int _numberOfRetries;
+
+        private static CommandlineOptions _options;
+        private static MessageHelper _message;
+        private static int _returnvalue;
+
+        internal static int Main(string[] args)
         {
-            Console.WriteLine("== FOUND THESE PORTS == ");
+            try
+            {
+                Parser.Default.ParseArguments<CommandlineOptions>(args)
+                                   .WithParsed<CommandlineOptions>(RunLogic)
+                                   .WithNotParsed(HandleErrors);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: Parsing arguments threw an exception with message `{ex.Message}`");
+                _returnvalue = 1;
+            }
+
+            Console.WriteLine($"Exit with return code {_returnvalue}");
+
+            return _returnvalue;
+        }
+
+        /// <summary>
+        /// On parameter errors, we set the returnvalue to 1 to indicated an error.
+        /// </summary>
+        /// <param name="errors">List or errors (ignored).</param>
+        private static void HandleErrors(IEnumerable<Error> errors)
+        {
+            _returnvalue = 1;
+        }
+
+        private static void RunLogic(CommandlineOptions o)
+        {
+            _options = o;
+            _message = new MessageHelper(_options);
+            string[] peFiles;
+            string workingDirectory;
+
+            // Let's first validate that the directory exist and contains PE files
+            _options.PeDirectory = $"{_options.PeDirectory}{Path.DirectorySeparatorChar}";
+            if (Directory.Exists(_options.PeDirectory))
+            {
+                workingDirectory = Path.GetDirectoryName(Path.Combine(_options.PeDirectory));
+                peFiles = Directory.GetFiles(workingDirectory, "*.pe");
+                if (peFiles.Length == 0)
+                {
+                    _message.Error("The target directory does not contain any PE file.");
+                    _returnvalue = 1;
+                    return;
+                }
+            }
+            else
+            {
+                _message.Error("The target directory is not a valid one.");
+                _returnvalue = 1;
+                return;
+            }
+
+            _message.Verbose("Finding valid ports");
 
             List<byte[]> assemblies = new List<byte[]>();
             int retryCount = 0;
-            int _numberOfRetries = 100;
-
+            _numberOfRetries = 10;
             var serialDebugClient = PortBase.CreateInstanceForSerial(true, null);
 
         retryConnection:
@@ -35,17 +86,20 @@ namespace nanoFrameworkFlasher
                 Thread.Sleep(1);
             }
 
-            Console.WriteLine($"Found: {serialDebugClient.NanoFrameworkDevices.Count} devices");
+            _message.Output($"Found: {serialDebugClient.NanoFrameworkDevices.Count} devices");
 
             if (serialDebugClient.NanoFrameworkDevices.Count == 0)
             {
                 if (retryCount > _numberOfRetries)
                 {
-                    Console.WriteLine("ERRROR TOO MANY RETRIES");
+                    _message.Error("Error too many retries");
+                    _returnvalue = 1;
+                    return;
                 }
                 else
                 {
                     retryCount++;
+                    _message.Verbose($"Finding devices, attempt {retryCount}");
                     serialDebugClient.ReScanDevices();
                     goto retryConnection;
                 }
@@ -54,41 +108,41 @@ namespace nanoFrameworkFlasher
             retryCount = 0;
             NanoDeviceBase device = serialDebugClient.NanoFrameworkDevices[0];
 
-            //BartJ TODO account for multiple connected devices: 
-            // if (serialDebugClient.NanoFrameworkDevices.Count > 1)
-            // {
-            //     device = serialDebugClient.NanoFrameworkDevices.Where(m => m.SerialNumber == port).First();
-            // }
-            // else
-            // {
-            //     device = serialDebugClient.NanoFrameworkDevices[0];
-            // }
+            // In case we have multiple ones, we will go for the one passed if the argument is valid
+            if ((serialDebugClient.NanoFrameworkDevices.Count > 1) && (!string.IsNullOrEmpty(_options.ComPort)))
+            {
+                device = serialDebugClient.NanoFrameworkDevices.Where(m => m.SerialNumber == _options.ComPort).First();
+            }
+            else
+            {
+                device = serialDebugClient.NanoFrameworkDevices[0];
+            }
 
-            Console.WriteLine($"Getting things with {device.Description}");
+            _message.Output($"Deploying on {device.Description}");
 
             // check if debugger engine exists
             if (device.DebugEngine == null)
             {
                 device.CreateDebugEngine();
-                Console.WriteLine($"Debug engine created.");
+                _message.Verbose($"Debug engine created.");
             }
 
         retryDebug:
             bool connectResult = device.DebugEngine.Connect(5000, true, true);
-            Console.WriteLine($"Device connect result is {connectResult}. Attempt {retryCount}/{_numberOfRetries}");
+            _message.Output($"Device connect result is {connectResult}. Attempt {retryCount}/{_numberOfRetries}");
 
             if (!connectResult)
             {
                 if (retryCount < _numberOfRetries)
                 {
                     // Give it a bit of time
-                    await Task.Delay(100);
+                    Thread.Sleep(100);
                     retryCount++;
                     goto retryDebug;
                 }
                 else
                 {
-                    Console.WriteLine("ERRROR TOO MANY RETRIES");
+                    _message.Error("Error too many retries");
                 }
             }
 
@@ -96,36 +150,33 @@ namespace nanoFrameworkFlasher
 
         retryErase:
             // erase the device
-            Console.WriteLine(($"Erase deployment block storage. Attempt {retryCount}/{_numberOfRetries}."));
+            _message.Output($"Erase deployment block storage. Attempt {retryCount}/{_numberOfRetries}.");
 
             var eraseResult = device.Erase(
                     EraseOptions.Deployment,
                     null,
                     null);
 
-            Console.WriteLine(($"Erase result is {eraseResult}."));
+            _message.Verbose($"Erase result is {eraseResult}.");
             if (!eraseResult)
             {
                 if (retryCount < _numberOfRetries)
                 {
                     // Give it a bit of time
-                    await Task.Delay(400);
+                    Thread.Sleep(400);
                     retryCount++;
                     goto retryErase;
                 }
                 else
                 {
-                    Console.WriteLine("COULDNT ERASE. TOO MANY RETRIES");
+                    _message.Error("Couldn't erase. Too many retries");
                 }
             }
 
             // build a list with the full path for each DLL, referenced DLL and EXE
             List<String> assemblyList = new List<String>();
 
-            var workingDirectory = Path.GetDirectoryName(@"/Users/bartjansen/htdocs/nanoFrameworkFlasher/bin/Debug/testPEs/");
-            var peFiles = Directory.GetFiles(workingDirectory, "*.pe");
-
-            Console.WriteLine($"Added {peFiles.Length} assemblies to deploy.");
+            _message.Verbose($"Added {peFiles.Length} assemblies to deploy.");
 
             // Keep track of total assembly size
             long totalSizeOfAssemblies = 0;
@@ -137,10 +188,10 @@ namespace nanoFrameworkFlasher
                 using (FileStream fs = File.Open(Path.Combine(workingDirectory, peItem), FileMode.Open, FileAccess.Read))
                 {
                     long length = (fs.Length + 3) / 4 * 4;
-                    Console.WriteLine($"Adding {peItem} v0 ({length} bytes) to deployment bundle");
+                    _message.Verbose($"Adding {peItem} v0 ({length} bytes) to deployment bundle");
                     byte[] buffer = new byte[length];
 
-                    await fs.ReadAsync(buffer, 0, (int)fs.Length);
+                    fs.Read(buffer, 0, (int)fs.Length);
                     assemblies.Add(buffer);
 
                     // Increment totalizer
@@ -148,25 +199,28 @@ namespace nanoFrameworkFlasher
                 }
             }
 
-            Console.WriteLine($"Deploying {peFiles.Length:N0} assemblies to device... Total size in bytes is {totalSizeOfAssemblies}.");
+            _message.Output($"Deploying {peFiles.Length:N0} assemblies to device... Total size in bytes is {totalSizeOfAssemblies}.");
 
             // need to keep a copy of the deployment blob for the second attempt (if needed)
             var assemblyCopy = new List<byte[]>(assemblies);
 
-            var deploymentLogger = new Progress<string>((m) => Console.WriteLine(m));
-        
+            var deploymentLogger = new Progress<string>((m) => _message.Output(m));
+            // Seems to be needed for slow devices
+            Thread.Sleep(200);
             if (!device.DebugEngine.DeploymentExecute(
                 assemblyCopy,
-                false,
+                _options.RebootAfterFlash,
                 false,
                 null,
                 deploymentLogger))
             {
-                Console.WriteLine("WRITE FAILED :(");
+                _message.Error("Write failed.");
+                _returnvalue = 1;
+                return;
             }
             else
             {
-                Console.WriteLine("WRITE SUCCEEDED :)");
+                _message.Output("Write successful");
             }
         }
     }
